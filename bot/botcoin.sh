54dbcd666444f11add0a4ba7730d5dbf6845b7b4
Sabino Velasquez <sabinovelasquez@gmail.com>
2017-12-13 11:31:23 -0300

    check process once

--- bot/botcoin.sh
+++ bot/botcoin.sh
@@ -9,58 +9,83 @@ price_temp=${price#'"'};
 price_temp=${price_temp%'"'};
 price_int=${price_temp%.*}
 
+
 if [ "$price_int" -le "$price_buy" ]
 then
-  curl -X POST https://textbelt.com/text \
-  --data-urlencode phone='+56968406912' \
-  --data-urlencode message='COMPRA Bitcoins CTM! La moneda está actualmente en: '$price_int \
-  -d key=62e3e2959d4f4905ab5035df2f6e17672c84386cNqcZCYuIWnL7GlGRd2RYMACl3
+  file="buy.check"
+  if [ -f "$file" ]
+  then
+    echo "$file found."
+  else
+    curl -X POST https://textbelt.com/text \
+    --data-urlencode phone='+56968406912' \
+    --data-urlencode message='COMPRA Bitcoins CTM! La moneda está actualmente en: '$price_int \
+    -d key=62e3e2959d4f4905ab5035df2f6e17672c84386cNqcZCYuIWnL7GlGRd2RYMACl3
 
-  curl -X POST https://textbelt.com/text \
-  --data-urlencode phone='+56984134906' \
-  --data-urlencode message='COMPRA Bitcoins CTM! La moneda está actualmente en: '$price_int \
-  -d key=62e3e2959d4f4905ab5035df2f6e17672c84386cNqcZCYuIWnL7GlGRd2RYMACl3
+    curl -X POST https://textbelt.com/text \
+    --data-urlencode phone='+56984134906' \
+    --data-urlencode message='COMPRA Bitcoins CTM! La moneda está actualmente en: '$price_int \
+    -d key=62e3e2959d4f4905ab5035df2f6e17672c84386cNqcZCYuIWnL7GlGRd2RYMACl3
 
-  curl -X POST https://textbelt.com/text \
-  --data-urlencode phone='+56988193592' \
-  --data-urlencode message='COMPRA Bitcoins CTM! La moneda está actualmente en: '$price_int \
-  -d key=62e3e2959d4f4905ab5035df2f6e17672c84386cNqcZCYuIWnL7GlGRd2RYMACl3
+    curl -X POST https://textbelt.com/text \
+    --data-urlencode phone='+56988193592' \
+    --data-urlencode message='COMPRA Bitcoins CTM! La moneda está actualmente en: '$price_int \
+    -d key=62e3e2959d4f4905ab5035df2f6e17672c84386cNqcZCYuIWnL7GlGRd2RYMACl3
+
+    cat > buy.check
+  fi
 fi
 
 if [ "$price_int" -ge "$price_high" ]
 then
-  curl -X POST https://textbelt.com/text \
-  --data-urlencode phone='+56968406912' \
-  --data-urlencode message='Salte AHORA! Ganaste. La moneda está actualmente en: '$price_int'. No perdai plata wn.' \
-  -d key=62e3e2959d4f4905ab5035df2f6e17672c84386cNqcZCYuIWnL7GlGRd2RYMACl3
+  file="out.check"
+  if [ -f "$file" ]
+  then
+    echo "$file found."
+  else
+    curl -X POST https://textbelt.com/text \
+    --data-urlencode phone='+56968406912' \
+    --data-urlencode message='Salte AHORA! Ganaste. La moneda está actualmente en: '$price_int'. No perdai plata wn.' \
+    -d key=62e3e2959d4f4905ab5035df2f6e17672c84386cNqcZCYuIWnL7GlGRd2RYMACl3
 
-  curl -X POST https://textbelt.com/text \
-  --data-urlencode phone='+56984134906' \
-  --data-urlencode message='Salte AHORA! Ganaste. La moneda está actualmente en: '$price_int'. No perdai plata wn.' \
-  -d key=62e3e2959d4f4905ab5035df2f6e17672c84386cNqcZCYuIWnL7GlGRd2RYMACl3
+    curl -X POST https://textbelt.com/text \
+    --data-urlencode phone='+56984134906' \
+    --data-urlencode message='Salte AHORA! Ganaste. La moneda está actualmente en: '$price_int'. No perdai plata wn.' \
+    -d key=62e3e2959d4f4905ab5035df2f6e17672c84386cNqcZCYuIWnL7GlGRd2RYMACl3
 
-  curl -X POST https://textbelt.com/text \
-  --data-urlencode phone='+56988193592' \
-  --data-urlencode message='Salte AHORA! Ganaste. La moneda está actualmente en: '$price_int'. No perdai plata wn.' \
-  -d key=62e3e2959d4f4905ab5035df2f6e17672c84386cNqcZCYuIWnL7GlGRd2RYMACl3
+    curl -X POST https://textbelt.com/text \
+    --data-urlencode phone='+56988193592' \
+    --data-urlencode message='Salte AHORA! Ganaste. La moneda está actualmente en: '$price_int'. No perdai plata wn.' \
+    -d key=62e3e2959d4f4905ab5035df2f6e17672c84386cNqcZCYuIWnL7GlGRd2RYMACl3
+
+    cat > out.check
+  fi
 fi
 
 if [ "$price_int" -le "$price_low" ]
 then
-  curl -X POST https://textbelt.com/text \
-  --data-urlencode phone='+56968406912' \
-  --data-urlencode message='Salte AHORA! La moneda está actualmente en: '$price_int'. Puede seguir bajando!' \
-  -d key=62e3e2959d4f4905ab5035df2f6e17672c84386cNqcZCYuIWnL7GlGRd2RYMACl3
+  file="sell.check"
+  if [ -f "$file" ]
+  then
+    echo "$file found."
+  else
+    curl -X POST https://textbelt.com/text \
+    --data-urlencode phone='+56968406912' \
+    --data-urlencode message='Salte AHORA! La moneda está actualmente en: '$price_int'. Puede seguir bajando!' \
+    -d key=62e3e2959d4f4905ab5035df2f6e17672c84386cNqcZCYuIWnL7GlGRd2RYMACl3
 
-  curl -X POST https://textbelt.com/text \
-  --data-urlencode phone='+56984134906' \
-  --data-urlencode message='Salte AHORA! La moneda está actualmente en: '$price_int'. Puede seguir bajando!' \
-  -d key=62e3e2959d4f4905ab5035df2f6e17672c84386cNqcZCYuIWnL7GlGRd2RYMACl3
+    curl -X POST https://textbelt.com/text \
+    --data-urlencode phone='+56984134906' \
+    --data-urlencode message='Salte AHORA! La moneda está actualmente en: '$price_int'. Puede seguir bajando!' \
+    -d key=62e3e2959d4f4905ab5035df2f6e17672c84386cNqcZCYuIWnL7GlGRd2RYMACl3
 
-  curl -X POST https://textbelt.com/text \
-  --data-urlencode phone='+56988193592' \
-  --data-urlencode message='Salte AHORA! La moneda está actualmente en: '$price_int'. Puede seguir bajando!' \
-  -d key=62e3e2959d4f4905ab5035df2f6e17672c84386cNqcZCYuIWnL7GlGRd2RYMACl3
+    curl -X POST https://textbelt.com/text \
+    --data-urlencode phone='+56988193592' \
+    --data-urlencode message='Salte AHORA! La moneda está actualmente en: '$price_int'. Puede seguir bajando!' \
+    -d key=62e3e2959d4f4905ab5035df2f6e17672c84386cNqcZCYuIWnL7GlGRd2RYMACl3
+
+    cat > sell.check
+  fi
 fi
 
 now=$(date +"%T")
@@ -72,3 +97,5 @@ cat <<EOF >bitcoin.json
   }
 }
 EOF
+
+echo "end of bot"
