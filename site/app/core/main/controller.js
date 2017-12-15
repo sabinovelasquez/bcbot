866ce658f2ba98c3da39ea5d53eb731f61543cb8
Sabino Velasquez <sabinovelasquez@gmail.com>
2017-12-15 11:44:17 -0300

    new json format

--- site/app/core/main/controller.js
+++ site/app/core/main/controller.js
@@ -1,7 +1,16 @@
 export default ngModule => {
-  ngModule.controller('MainCtrl', function MainCtrl(surbtcAPIService) {
-    surbtcAPIService.getBTC().then(response => {
-      this.surbtc = response.data.ticker;
-    });
+  ngModule.controller('MainCtrl', function MainCtrl(surbtcAPIService, $timeout) {
+    this.surbtcAPIService = surbtcAPIService;
+    this.loading = true;
+    this.update = () => {
+      this.surbtcAPIService.getBTC().then(response => {
+        const surbtc = response.data.botcoin;
+        this.bitcoin = surbtc.btc_clp;
+        this.ether = surbtc.eth_clp;
+        this.last_check = surbtc.time;
+        this.loading = false;
+      });
+    };
+    $timeout(this.update(), 60000);
   });
 };
